461d1a4576e9ffb1424b73fe763cf0c3ec57c8c2
shuuryou <36278767+shuuryou@users.noreply.github.com>
2017-02-08 12:40:08 +0100

    Get rid of FLV
    Flash is dead, so instead of FLV live streaming, use h264.

--- htdocs/live.php
+++ htdocs/live.php
@@ -12,7 +12,7 @@ if ($Enabled !== 1)
 {
 	http_response_code(403);
 	echo 'Live streaming is disabled.';
-	exit();
+	return;
 }
 
 $cameras = BuildCameraList(); // Not escaped
@@ -24,7 +24,7 @@ if (!in_array($_GET['camera'], $cameras))
 {
 	http_response_code(404);
 	echo 'Camera does not exist.';
-	exit();
+	return;
 }
 
 $stream = GetSetting($camera .'.livestream');
@@ -36,7 +36,7 @@ if (empty($stream))
 {
 	http_response_code(500);
 	echo 'H264 stream for camera is missing.';
-	exit();
+	return;
 }
 
 $command = GetSetting('webinterface.streamcommand');
@@ -45,7 +45,7 @@ if (empty($command))
 {
 	http_response_code(500);
 	echo 'Grabber command for web interface is missing.';
-	exit();
+	return;
 }
 
 $command = str_replace('{STREAM}', escapeshellarg($stream), $command);
@@ -56,6 +56,6 @@ while (ob_get_level() > 0 && ob_end_clean());
 
 header('Transfer-Encoding: chunked');
 header('Connection: close');
-header('Content-Type: video/x-flv');
+header('Content-Type: video/mp4');
 
 passthru($command);
